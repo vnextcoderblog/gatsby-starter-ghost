b97dfbcd9f3e89b5cb38b6daaa51a4665f4e5e06
Avinash Barnwal <vnextcoder@gmail.com>
2021-02-26 21:50:14 +0530

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://confident-pasteur-8c72cd.netlify.app/`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://objective-fermat-9bd7e1.netlify.app/`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
